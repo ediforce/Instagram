aeb6b21a50156cab0398494558c9fa3e23ce522b
K1 <keiichi@funatsukin.com>
2018-05-08 09:38:04 +0900

    課題開始前の状態だが、原因不明のエラーが発生している。

---
+++ Instagram/Const.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  Instagram
+//
+//  Created by Keiichi Funatsuki on 2018/05/07.
+//  Copyright © 2018年 keiichi.funatsuki. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let PostPath = "posts"
+    
+}
